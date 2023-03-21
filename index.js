255ca99ee5379313aa2eee5f0f19a169d8d85823
Muhammad Iqbal Masykuri <muhammadiqbal2020@mail.ugm.ac.id>
2023-03-21 14:14:42 +0700

    fix header in response

--- index.js
+++ index.js
@@ -25,6 +25,24 @@ mongoose
     process.exit(1);
   });
 
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
